5516f63e04e6ad269f38aac3d4a6d10bd35d65c4
Aimee <aimeeharkin@gmail.com>
2021-10-05 15:56:50 +0200

    completed all, except the last fetch request.

---
+++ client/client/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+
+  const courses = () => {
+    fetch('http://localhost:5000/html', {
+    method: "GET"
+    })
+      .then(res => res.json())
+      .then(json => console.log(json))
+      .catch(err => console.log('Request failed', err))
+  }
+  
+
+  return (
+    <div className="App">
+      <button onClick={courses}>Click to see courses</button>
+    </div>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+const cors = require('cors')
 const express = require('express')
 const app = express()
+
+app.use( cors() )
+
+const port = 5000
 
 const coursesArr = [ 'HTML', 'CSS', "JavaScript", 'DOM', "SPA", 'Backend', 'Fullstack', "Final Project" ]
 
@@ -10,7 +15,43 @@ app.get('/json', (req, res) => {
 
 })
 
-app.listen(5000, () => {
+// BONUS
+
+app.get('/text', (req, res) => {
+    res.json(coursesArr.join())
+})
+
+app. get('/html', (req, res) => {
+
+    const list = coursesArr.map(item => {
+        if(item == "Backend"){
+         return '<li><b>' + item + '</b></li>'
+        }else{
+         return   '<li>' + item + '</li>'
+        }
+    })
+
+    console.log(list);
+
+    const html = '<ul>' + list.concat().join('') + '</ul>'
+
+    res.send(html)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
     console.log('API has successfully started in PORT 5000');
 })
 
